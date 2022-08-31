7b107ef27f67b9877339bcc9100227fdaf74d4c0
Byell00123 <112014752+Byell00123@users.noreply.github.com>
2022-08-31 18:51:03 -0300

    Update App.java

--- Banco/src/App.java
+++ Banco/src/App.java
@@ -3,69 +3,90 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        Scanner banana = new Scanner(System.in);
-        Poupanca p = new Poupanca();
-        CorrenteComum cc = new CorrenteComum();
-        
-        
-        
-        
-        /* p.depositar(100.0);
-        p.getSaldo();
-        p.sacar(100.);
-        p.getSaldo();
-        cc.depositar(100.);
-        cc.getSaldo();
-        cc.sacar(50.);
-        System.out.println("x");
-        cc.getSaldo(); */
-        
-        System.out.printf("Quanto você tem em conta: ");
+            Scanner banana = new Scanner(System.in);
+            Poupanca p = new Poupanca();
+            CorrenteComum cc = new CorrenteComum();
+            ClienteEspecial ce = new ClienteEspecial();
 
+            System.out.printf("Saldo atual R$ 0,0");
 
         while (true){
-            int ac=0;
-            System.out.printf("\nQual o tipo da sua conta:\n1-Poupança\n2-Corrente Comum\n3-Conta Especial\n:");
-
-            System.out.printf("\nQue ação deseja tomar: \n1-Depositar.\n2-Sacar.\nSair-Qualquer outro numero.\n:");
-            ac = banana.nextInt();
-                
-            
-            
-            
-            
-            
-            
-            
-            switch(ac) {
+            int tipo_conta;
+            System.out.printf("\nQual o tipo da sua conta:\n1-Poupança\n2-Corrente Comum\n3-Cliente especial.\n4-Sair\n:");
+            tipo_conta=banana.nextInt();
+            int ac;
+            switch (tipo_conta) {
                 case 1:
-                    System.out.printf("\nQuanto você deseja depositar: ");
-                    p.setSaldo(banana.nextDouble());
-                    break;
-                case 2:
-                    int tipo_conta=0;
-                    System.out.printf("\nQual o tipo da sua conta:\n1-Poupança\n2-Corrente Comum\n3-Conta Especial\n:");
-                    tipo_conta=banana.nextInt();
-                    switch (tipo_conta) {
+                    System.out.printf("\nQue ação deseja tomar: \n1-Depositar.\n2-Sacar.\n3-Extrato.\nTocar de Conta-Qualquer outro numero.\n:");
+                    ac = banana.nextInt();
+                    switch (ac) {
                         case 1:
-                            System.out.printf("\nQuanto você deseja sacar: ");
-                            p.sacar(banana.nextDouble());
+                            System.out.printf("\nQuanto você deseja depositar: ");
+                            p.depositar(banana.nextDouble());
                             break;
                         case 2:
                             System.out.printf("\nQuanto você deseja sacar: ");
-                            cc.sacar(banana.nextDouble());
-                                ///////
-                        
+                            p.sacar(banana.nextDouble());
+                            break;
+                        case 3:
+                            p.extrato();
+                            break;
                         default:
                             break;
                     }
                     break;
-                default:
+/*-------------------------------------------------------------------------------------------------------------------*/
+                case 2:
+                    System.out.printf("\nQue ação deseja tomar: \n1-Depositar.\n2-Sacar.\n3-Extrato.\nTocar de Conta-Qualquer outro numero.\n:");
+                    ac = banana.nextInt();
+                    switch (ac) {
+                        case 1:
+                            System.out.printf("\nQuanto você deseja depositar: ");
+                            cc.depositar(banana.nextDouble());
+                            break;
+                        case 2:
+                            System.out.printf("\nQuanto você deseja sacar: ");
+                            cc.sacar(banana.nextDouble());
+                            break;
+                        case 3:
+                            cc.extrato();
+                            break;
+                        default:
+                            break;
+                    }
+                    break;
+/*-------------------------------------------------------------------------------------------------------------------*/
+                case 3:
+                    System.out.printf("\nQue ação deseja tomar: \n1-Depositar.\n2-Sacar.\n3-Extrato.\nTocar de Conta-Qualquer outro numero.\n:");
+                    ac = banana.nextInt();
+                    switch (ac) {
+                        case 1:
+                            System.out.printf("\nQuanto você deseja depositar: ");
+                            ce.depositar(banana.nextDouble());
+                            break;
+                        case 2:
+                            System.out.printf("\nQuanto você deseja sacar: ");
+                            ce.sacar(banana.nextDouble());
+                            break;
+                        case 3:
+                            ce.extrato();
+                            break;
+                        default:
+                            break;
+                }
+                    break;
+/*-------------------------------------------------------------------------------------------------------------------*/
+                case 4:
                     System.exit(0);
-            }
-            
+                    break;
+/*-------------------------------------------------------------------------------------------------------------------*/
+                default:
+                    break;
+                        
+
+                }
+
         }
-    
-        
+                
     }
 }
